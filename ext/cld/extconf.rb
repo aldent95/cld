d96edb6559f70250eabd84c9c69a3a376b46ce86
Alex Dent <aldent95@users.noreply.github.com>
2023-04-26 15:59:56 +1200

    Update extconf.rb

--- ext/cld/extconf.rb
+++ ext/cld/extconf.rb
@@ -2,11 +2,13 @@ ENV['CFLAGS'] = ENV['CFLAGS'].to_s + ' -Wno-narrowing'
 ENV['CXXFLAGS'] = ENV['CXXFLAGS'].to_s + ' -Wno-narrowing'
 
 require 'mkmf'
-create_makefile('cld/cld')
+
 
 $defs.push("-DWIND32")
 $defs.push("-DCLD_WINDOWS")
 $defs.push("-DNDEBUG")
 $defs.push("-D_LIB")
+
+create_makefile('cld/cld')
 
 system "./configure --prefix=#{Dir.pwd}" unless File.exist?('Makefile')
